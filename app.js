58b8666fa1d2c12916fb6c5810c854d32e83be57
Burak Kuru <burak.kuru@abkholding.com>
2022-11-21 13:22:02 +0300

    modules-require

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
+
+var counter = require('./count')
+
+console.log(counter([1,2,3]));
+
+/**
+
 console.log('hi there.!');
+console.log(__dirname);
+console.log(__filename);
+
+var time= 0;
+
+var timer = setInterval(function(){
+
+    time += 2;
+    console.log(time + ' secound passed.')
+    if (time > 5) {
+        clearInterval(timer);
+    }
+},2000)
+
+function sayMyName(){
+    console.log('myname');""
+}
+
+function callFunction(func) {
+
+    func();
+}
+
+var sayHi = function(){
+
+    console.log('hi')
+}
+
+callFunction(sayHi);
+
+//require, exports
+
+ */
+
+
+
---
+++ count.js
@@ -0,0 +1,7 @@
+var counter = function(arr) {
+
+    return 'this arr has ' + arr.length + ' element';
+
+}
+
+module.exports = counter;

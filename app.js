8db66ab82fba4df38545bbb5e047db248441714b
Burak Kuru <burak.kuru@abkholding.com>
2022-11-25 13:16:14 +0300

    readable streams

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('hi there.!');
+var fs = require('fs');
+
+
+var myReadableStream = fs.createReadStream(__dirname + '/mytextfile.txt', 'utf8');
+
+
+myReadableStream.on('data',function(chunk){
+
+    console.log('new chunk received:');
+    //console.log(chunk); // we can see the chunk of data 
+});

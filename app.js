39b84628fa552c7c4baee66764cddb12865f9e2e
Burak Kuru <burak.kuru@abkholding.com>
2022-11-25 13:24:44 +0300

    writable-streams

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var fs = require('fs');
 
 
-var myReadableStream = fs.createReadStream(__dirname + '/mytextfile.txt', 'utf8');
+var myReadableStream = fs.createReadStream(__dirname + '/mytextfilefrom.txt', 'utf8');
+var myWritableStream = fs.createWriteStream(__dirname + '/mytextfileto.txt');
+
 
 
 myReadableStream.on('data',function(chunk){
 
     console.log('new chunk received:');
     //console.log(chunk); // we can see the chunk of data 
+
+    myWritableStream.write(chunk);
+
 });

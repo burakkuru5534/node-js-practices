da6845f54600eef2ac0790eeebb85f58de15d5d2
Burak Kuru <burak.kuru@abkholding.com>
2022-11-25 11:31:24 +0300

    directory-operations. mkdir, rmdir, unlink

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('hi there.!');
+var fs = require('fs');
+
+
+// fs.unlink('writeMe.txt'); // be sure if file exists or not. if it's not exists then it would throw an error.
+
+/*
+
+fs.mkdir('stuff',function(){
+    fs.readFile('readMe.txt','utf8',function(err,data){
+
+        fs.writeFile('./stuff/writeMe.txt',data, function(err,data){
+
+        });
+    });
+});
+
+*/
+
+// we should empty the directory first otherwise it would throw an error
+fs.unlink('./stuff/writeMe.txt',function(){
+    fs.rmdir('stuff', function(){});
+})

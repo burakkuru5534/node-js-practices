be0ab8cb82a70678acc15f98fe1cb7ea0c7ec642
Burak Kuru <burak.kuru@abkholding.com>
2022-11-25 15:22:22 +0300

    pipes. otomatically send data to write stream

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 var fs = require('fs');
+var http = require('http');
 
 
-var myReadableStream = fs.createReadStream(__dirname + '/mytextfilefrom.txt', 'utf8');
-var myWritableStream = fs.createWriteStream(__dirname + '/mytextfileto.txt');
+var server = http.createServer(function(req,res){
 
-
-
-myReadableStream.on('data',function(chunk){
-
-    console.log('new chunk received:');
-    //console.log(chunk); // we can see the chunk of data 
-
-    myWritableStream.write(chunk);
-
+    console.log('request was made', req.url);
+    res.writeHead(200,{'Content-Type': 'text/plain'});
+    var myReadableStream = fs.createReadStream(__dirname + '/mytextfilefrom.txt', 'utf8');
+    myReadableStream.pipe(res); 
+    // we can use pipe with only readable streams. it provides to make otomatically transfer data when buffer get fulled 
+    // otherwise we used to do that with readablestream.on and define a callback function and send that data manually.
 });
+
+server.listen(3000,'127.0.0.1');
+console.log('listening port: ' + 3000);

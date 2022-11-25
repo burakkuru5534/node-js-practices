57314cde20b4c06e00c0f1e13828eff05ba281c9
Burak Kuru <burak.kuru@abkholding.com>
2022-11-25 16:12:29 +0300

    basic routing

--- app.js
+++ app.js
@@ -5,18 +5,23 @@ var fs = require('fs');
 var server = http.createServer(function(req,res){
 
     console.log('request was made: ', req.url);
-    res.writeHead(200,{'COntent-Type':'application/json'});
-   
-   myObj = {
 
-    name:'burak',
-    job: 'officer',
-    age: 27
+    if (req.url === '/' || req.url === '/home'){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        fs.createReadStream(__dirname + '/index.html').pipe(res);
+    } else if (req.url === '/contact'){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        fs.createReadStream(__dirname + '/contact.html').pipe(res);
+    }else if (req.url === '/api/ninjas'){
+        res.writeHead(200,{'Content-Type':'application/json'});
 
-   }
-    res.end(JSON.stringify(myObj));
-
-})
+        ninjas = [{name:'burak','job':'ninja',age:27},{name:'marina','job':'ninja',age:28}]
+        res.end(JSON.stringify(ninjas));
+    }else {
+        res.writeHead(200,{'Content-Type':'text/html'});
+        fs.createReadStream(__dirname + '/404.html').pipe(res);
+    }
+});
 
 server.listen(3000, '127.0.0.1');
 console.log('listening port: ' + 3000);
